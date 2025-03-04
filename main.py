ff9a86c8b56393a681b66569bf932ce09c52f80c
OptimusBufkin <bufkinj11@gmail.com>
2025-03-04 14:25:22 -0700

    Move Player game loop calls to groups

--- main.py
+++ main.py
@@ -12,22 +12,23 @@ def main():
     window = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
     time = pygame.time.Clock()
     dt = 0
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
+        updatable.update(dt)
         pygame.Surface.fill(window, color=(0, 0, 0))
-        player.draw(window)
+        for item in drawable:
+            item.draw(window)
         dt = time.tick(60) / 1000
         pygame.display.flip()
-
-
-    # print("Starting Asteroids!")
-    # print(f"Screen width: {SCREEN_WIDTH}")
-    # print(f"Screen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
