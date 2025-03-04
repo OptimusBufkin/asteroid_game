c0922b55a89d2fdd36da4588632bd8954c7c8270
OptimusBufkin <bufkinj11@gmail.com>
2025-03-04 11:48:51 -0700

    Add Player class

--- main.py
+++ main.py
@@ -4,17 +4,23 @@ TODO: DOCSTRING
 import pygame
 
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
     window = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
+    time = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(window, color=(0, 0, 0))
+        player.draw(window)
+        dt = time.tick(60) / 1000
         pygame.display.flip()
 
 
